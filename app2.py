f409166496e55f34e0f2065451176a81a5d2dfba
Masood Khan <masoodkhanse28@gmail.com>
2025-02-10 14:39:10 +0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pickle
 import pandas as pd
-import scikit-learn as sklearn
+import sklearn
 from sklearn.preprocessing import StandardScaler
 from PIL import Image
 
